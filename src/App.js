1b12ecff9ed9ec9dc9cb2c13764131edd72f0b29
Cameron LaFerney <chlaferney@gmail.com>
2020-07-09 13:54:58 -0500

    Initialize project.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className={styles.appHeader}>Star Wars Planets</header>
+    </div>
+  );
+}
+
+export default App;
